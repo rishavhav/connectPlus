b99ea00fc80b82319d1927561ee9800e2217817b
risha <rs1470@g.rit.edu>
2023-03-29 21:21:56 -0400

    first commit for basic structure

--- base/views.py
+++ base/views.py
@@ -13,7 +13,7 @@ from .forms import RoomForm
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
-    rooms = Room.objects.filter(topic__name__icontains=q)
+    rooms = Room.objects.filter(topic__name__icontains=q, name_ic )
     topics = Topic.objects.all()
     context = {'rooms': rooms, 'topics': topics}
     return render(request, 'base/home.html', context)
