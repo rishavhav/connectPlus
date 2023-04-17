dfc165481588f1368195d6a19915d3be6a9fe826
risha <rs1470@g.rit.edu>
2023-04-17 18:34:39 -0400

    first commit for basic structure

--- base/views.py
+++ base/views.py
@@ -3,23 +3,15 @@ from django.http import HttpResponse
 from .models import Room, Topic
 from .forms import RoomForm
 
-
-# rooms = [
-#     {'id': 1, 'name': 'Lets learn Python'},
-#     {'id': 2, 'name': 'Design with me'},
-#     {'id': 3, 'name': 'Frontend Developers'}
-# ]
-
-
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
-    rooms = Room.objects.filter(topic__name__icontains=q, name_ic )
+    rooms = Room.objects.all()
     topics = Topic.objects.all()
     context = {'rooms': rooms, 'topics': topics}
     return render(request, 'base/home.html', context)
 
 
-def room(request, pk):
+def  room(request, pk):
     room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request, 'base/room.html', context)
